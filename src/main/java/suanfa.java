becc4089f8973a2aa49db29d6cd8a1be65f8b0f5
heweijiqn <95403358+heweijiqn@users.noreply.github.com>
2024-05-27 23:32:08 +0800

    启动

---
+++ src/main/java/suanfa.java
@@ -0,0 +1,12 @@
+/**
+ * @author 何伟健
+ * @version 1.0
+ * @date 2024/5/27 23:30
+ */
+
+
+public class suanfa {
+    public static void main(String[] args) {
+        System.out.println("算法启动");
+    }
+}
